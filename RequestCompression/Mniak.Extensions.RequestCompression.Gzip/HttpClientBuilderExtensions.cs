ae77d36e48b29b0776d4ebca852027c243deebdb
Andre Soares <andregsilv@gmail.com>
2021-02-08 19:00:22 -0300

    I'm going crazy with so many projects for a very small feature

---
+++ RequestCompression/Mniak.Extensions.RequestCompression.Gzip/HttpClientBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Example.Client.Infrastructure.Compression;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class HttpClientBuilderExtensions
+    {
+        public static IHttpClientBuilder AddGzipCompression(this IHttpClientBuilder builder, string contentEncoding = "gzip")
+        {
+            builder.AddHttpMessageHandler(() => new GzipMessageHandler(contentEncoding));
+            return builder;
+        }
+    }
+}
